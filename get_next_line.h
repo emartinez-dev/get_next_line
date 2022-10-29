d382a1f819a9513b9678c6ecc204b833b89fddc8
emartinez <enri.fc.98@gmail.com>
2022-10-29 23:33:40 +0200

    ADD: define buffer_size in header if not defined

--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,18 @@
 /*   By: franmart <franmart@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 16:58:16 by franmart          #+#    #+#             */
-/*   Updated: 2022/10/09 19:03:45 by franmart         ###   ########.fr       */
+/*   Updated: 2022/10/29 23:32:24 by franmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #	ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# elif BUFFER_SIZE < 1
+#  error "BUFFER_SIZE must be a positive integer"
+# endif
 
 # include <unistd.h>
 # include <stdlib.h>
--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,12 +6,18 @@
 /*   By: franmart <franmart@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 16:09:11 by franmart          #+#    #+#             */
-/*   Updated: 2022/10/10 16:11:41 by franmart         ###   ########.fr       */
+/*   Updated: 2022/10/29 23:32:44 by franmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #	ifndef GET_NEXT_LINE_BONUS_H
 # define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# elif BUFFER_SIZE < 1
+#  error "BUFFER_SIZE must be a positive integer"
+# endif
 
 # include <unistd.h>
 # include <stdlib.h>
