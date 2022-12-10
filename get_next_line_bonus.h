ae7f5619c386557f00c272422e034715b21c4efa
emartinez <enri.fc.98@gmail.com>
2022-12-10 18:10:44 +0100

    FIX: function name

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,7 +6,7 @@
 /*   By: franmart <franmart@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 16:09:11 by franmart          #+#    #+#             */
-/*   Updated: 2022/12/10 18:01:54 by franmart         ###   ########.fr       */
+/*   Updated: 2022/12/10 18:06:19 by franmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,5 @@
 # include <limits.h>
 
 char			*ft_gnl(int fd);
-unsigned int	ft_strlen(const char *s);
-char			*ft_strjoin(char *s1, char *s2);
-int				ft_strchr(const char *s, int c);
-void			ft_bzero(void *s, unsigned int n);
-void			*ft_calloc(unsigned int nmemb, unsigned int size);
 
 #	endif
